b5cd75c437607a4473d299e63b44d6a2eaba8e42
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-08-31 19:29:26 -0300

    Create Medico.ts

---
+++ Codigo/Classes/Medico.ts
@@ -0,0 +1,18 @@
+export default class Medico{
+    
+    constructor(crm: String, nome: String, tel: number, esp: String){
+        this.crm = crm;
+        this.nome = nome;
+        this.telefone = tel;
+        this.especialidade = esp;
+    }
+    
+    crm: String;
+    nome: String;
+    telefone: number;
+    especialidade: String;
+}
+
+let med1 = new Medico("12345SP", "joao", 98898989, "cardicao");
+
+console.log(`Nome: ${med1.nome}, CRM: ${med1.crm}, tel: ${med1.telefone}, esp: ${med1.especialidade}`);
