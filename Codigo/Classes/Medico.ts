51e9121b443f0d95982c2ca4c7c1c7ac447c07f7
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-08-31 20:16:42 -0300

    Update Medico.ts

--- Codigo/Classes/Medico.ts
+++ Codigo/Classes/Medico.ts
@@ -1,18 +1,45 @@
 export default class Medico{
+
+    private crm: String;
+    private nome: String;
+    private telefone: number;
+    private especialidade: String;
     
+    //construtor da classe
     constructor(crm: String, nome: String, tel: number, esp: String){
         this.crm = crm;
         this.nome = nome;
         this.telefone = tel;
         this.especialidade = esp;
     }
+
+    //metodos get e set. Fazer para os outros atributos tbm
+    //Como faz para mais de um?
+    public getMedicoCRM(){
+        return this.crm;
+    }
+
+    public setMedicoCRM(crm : String){
+        this.crm = crm;
+    }
     
-    crm: String;
-    nome: String;
-    telefone: number;
-    especialidade: String;
+    public getInfoMed(){
+        console.log(`Nome: ${this.nome}, CRM: ${this.getMedicoCRM}, tel: ${this.telefone}, esp: ${this.especialidade}`);
+    }
+
+
+    //exemplo de funcao qualquer
+    public chamarMedico(crm: String): void{
+    } 
 }
 
 let med1 = new Medico("12345SP", "zezim", 98898989, "cardicao");
 
-console.log(`Nome: ${med1.nome}, CRM: ${med1.crm}, tel: ${med1.telefone}, esp: ${med1.especialidade}`);
+med1.setMedicoCRM("98765SP");
+console.log(med1.getMedicoCRM());
+med1.getInfoMed;
+
+//Jeito sem private
+//console.log(`Nome: ${med1.nome}, CRM: ${med1.crm}, tel: ${med1.telefone}, esp: ${med1.especialidade}`);
+
+
