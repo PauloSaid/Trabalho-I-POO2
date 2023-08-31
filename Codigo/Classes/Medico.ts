fd4e395461319aea7541261505d2498a435baeef
Matheus <100313664+Matheusmslopes@users.noreply.github.com>
2023-08-31 19:29:50 -0300

    Update Medico.ts

--- Codigo/Classes/Medico.ts
+++ Codigo/Classes/Medico.ts
@@ -13,6 +13,6 @@ export default class Medico{
     especialidade: String;
 }
 
-let med1 = new Medico("12345SP", "joao", 98898989, "cardicao");
+let med1 = new Medico("12345SP", "zezim", 98898989, "cardicao");
 
 console.log(`Nome: ${med1.nome}, CRM: ${med1.crm}, tel: ${med1.telefone}, esp: ${med1.especialidade}`);
